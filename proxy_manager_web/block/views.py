a205e1fa491b05ac451b733edc54643734db7eb5
eduardoporfiro <eduardo.porfiro.costa.1998@gmail.com>
2018-07-12 21:05:15 -0300

    inicio

--- proxy_manager_web/block/views.py
+++ proxy_manager_web/block/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from block.models import Teste
 
 def index(request):
-    return render(request, 'block/index.html')
+    testes = Teste.objects.all()
+    template = loader.get_template('block/index.html')
+    context = {
+        'testes': testes,
+    }
+    return HttpResponse(template.render(context, request))
