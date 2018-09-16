43a3651b659d4fd2500bbd322ffd244925e8876e
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2018-09-15 22:57:56 -0300

    TABS - 4

--- proxy_manager_web/block/tables.py
+++ proxy_manager_web/block/tables.py
@@ -6,6 +6,6 @@ class MqttTable(tables.Table):
     Editar = tables.LinkColumn('block:edit_mqtt', args=[A('pk')], empty_values=list(),text='Editar')
     class Meta:
         model = Mqtt
-        template_name = 'django_tables2/bootstrap.html'
+        template_name = 'django_tables2/bootstrap-responsive.html'
         sequence = ('id','topico', 'broker', 'QoS','RC', 'proxy')
         empty_text = "Sem MQTT cadastrado para o Broker"
