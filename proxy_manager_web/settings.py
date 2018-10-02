73a411213fa89a5a395dfd5d42910aa977f8c173
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2018-10-02 20:03:13 -0300

    HEROKU

--- proxy_manager_web/settings.py
+++ proxy_manager_web/settings.py
@@ -152,7 +152,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #Celery Config
 #BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-BROKER_URL = os.environ.get("CLOUDAMQP_URL", "")
+#BROKER_URL = os.environ.get("CLOUDAMQP_URL", "")
+BROKER_URL = 'amqp://kljpxmmt:8DOmdw2f5qzc-QshcdMXN1orLt7F_vpu@woodpecker.rmq.cloudamqp.com/kljpxmmt'
 BROKER_POOL_LIMIT = 1
 BROKER_HEARTBEAT = None
 BROKER_CONNECTION_TIMEOUT = 30
