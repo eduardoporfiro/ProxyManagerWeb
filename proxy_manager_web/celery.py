465243ab45d131f5d7c2d8b3383593419c965447
Eduardo Costa <eduardo.porfiro.costa.1998@gmail.com>
2018-10-03 07:53:25 -0300

    heroku-redis

--- proxy_manager_web/celery.py
+++ proxy_manager_web/celery.py
@@ -4,5 +4,6 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proxy_manager_web.settings')
 
 app = Celery('proxy_manager_web')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 app.autodiscover_tasks()
