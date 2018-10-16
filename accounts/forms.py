4dd41ed295a4c5f1ac80adefc7e7bab03cd44b78
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2018-10-15 21:03:23 -0300

    tarefa-job

--- accounts/forms.py
+++ accounts/forms.py
@@ -49,7 +49,7 @@ class PasswordResetForm(forms.Form):
         reset = PasswordReset(key=key, user=user)
         reset.save()
         template_name = 'accounts/password_reset_mail.html'
-        subject = 'Criar nova senha no Simple MOOC'
+        subject = 'Criar nova senha ProxyManagerWeb'
         context = {
             'reset': reset,
         }
