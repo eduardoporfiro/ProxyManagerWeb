1d600b7753c50ea3b5db4f3135519004614ad89a
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2019-01-08 21:39:55 -0200

    ELSE

--- tarefa/static/tarefa/tarefa_dados.js
+++ tarefa/static/tarefa/tarefa_dados.js
@@ -14,6 +14,7 @@ $(document).on('click', "a[dados='true']", function(event) {
 });
 
 function alterDATA(url, id, graphUrl) {
+    waitingDialog.show('Carregando');
     var graph_id = 'graph-'+id;
     if(! id.includes('tabela'))
         id = 'tabela-'+id;
@@ -86,6 +87,7 @@ function alterDATA(url, id, graphUrl) {
     });
 
     grafico(graphUrl, graph_id);
+setTimeout(function () {waitingDialog.hide();}, 1000);
 }
 function grafico(url, graph_id) {
     $.ajax({                       // initialize an AJAX request
@@ -140,3 +142,64 @@ function grafico(url, graph_id) {
         }
     });
 }
+
+
+var waitingDialog = waitingDialog || (function ($) {
+    'use strict';
+
+	// Creating modal dialog's DOM
+	var $dialog = $(
+		'<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+		'<div class="modal-dialog modal-m">' +
+		'<div class="modal-content">' +
+			'<div class="modal-header"><h3 style="margin:0;"></h3><i class="fa fa-cog fa-spin fa-3x fa-fw" style="display: block; width: 50%;"></div>' +
+			'<div class="modal-body">' +'</div>' +
+		'</div></div></div>');
+
+	return {
+		/**
+		 * Opens our dialog
+		 * @param message Custom message
+		 * @param options Custom options:
+		 * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
+		 * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
+		 */
+		show: function (message, options) {
+			// Assigning defaults
+			if (typeof options === 'undefined') {
+				options = {};
+			}
+			if (typeof message === 'undefined') {
+				message = 'Loading';
+			}
+			var settings = $.extend({
+				dialogSize: 'm',
+				progressType: '',
+				onHide: null // This callback runs after the dialog was hidden
+			}, options);
+
+			// Configuring dialog
+			$dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
+			$dialog.find('.progress-bar').attr('class', 'progress-bar');
+			if (settings.progressType) {
+				$dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
+			}
+			$dialog.find('h3').text(message);
+			// Adding callbacks
+			if (typeof settings.onHide === 'function') {
+				$dialog.off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
+					settings.onHide.call($dialog);
+				});
+			}
+			// Opening dialog
+			$dialog.modal();
+		},
+		/**
+		 * Closes dialog
+		 */
+		hide: function () {
+			$dialog.modal('hide');
+		}
+	};
+
+})(jQuery);
