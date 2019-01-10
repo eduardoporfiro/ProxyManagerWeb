518742b8473afe6393851ca05adb0cded01caa33
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2019-01-09 23:12:57 -0200

    ELSE

--- tarefa/filters.py
+++ tarefa/filters.py
@@ -1,21 +1,21 @@
 import django_filters
-from django_filters import DateTimeFromToRangeFilter
+from django_filters import DateTimeFromToRangeFilter, ModelChoiceFilter
 from django_filters.widgets import RangeWidget
 from .models import Dado, Dispositivo
+
+
+def dispositivo(request):
+    dispo_pk = str.split(request.path_info, '/')[3]
+    dispos = Dispositivo.objects.get(pk=dispo_pk)
+    return Dispositivo.objects.filter(proxy=dispos.proxy.pk, tipo=1).all()
 
 
 class DadoFilter(django_filters.FilterSet):
     date_range = DateTimeFromToRangeFilter(field_name='date', widget=RangeWidget(
         attrs={'placeholder': 'DD/MM/YYYY hh:mm'}))
 
+    sensor = ModelChoiceFilter(queryset=dispositivo)
+
     class Meta:
         model = Dado
-        fields = ['sensor', 'valor_char', 'valor_int']
-
-    @property
-    def qs(self):
-        dado = super(DadoFilter, self).qs
-        dispo_pk = str.split(self.request.path_info, '/')[3]
-        dispos = Dispositivo.objects.get(pk=dispo_pk)
-        dispos = Dispositivo.objects.filter(proxy=dispos.proxy.pk).all()
-        return dado.filter(sensor__in=dispos)
+        fields = ['valor_char', 'valor_int']
