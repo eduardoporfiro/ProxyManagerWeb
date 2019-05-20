b6ef4a5d0d334fa175fefcb0c0e944a6e52ba410
eduardoporfiro <eduardo.porfiro.costa.1998@gmail.com>
2019-05-20 20:00:56 -0300

    HEROKU - 5

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Celery)
-admin.site.site_title = 'Aquele Teste'
-admin.site.site_header = 'outro Teste'
-admin.site.index_title = 'outro Teste ao quadrado'
+admin.site.site_title = 'Proxy Manager Web'
+admin.site.site_header = 'Proxy Manager Web'
+admin.site.index_title = 'Administração do ProxyManager Web'
